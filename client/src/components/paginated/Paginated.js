f5864f228f24bf2ba87b77e578b822fa3d0b4239
Pablo Szejpiacki <molder26@hotmail.com>
2022-07-04 19:20:15 -0300

    added paginated styles

--- client/src/components/paginated/Paginated.js
+++ client/src/components/paginated/Paginated.js
@@ -1,22 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import "./Paginated.scss"
 
 export default function Paginated({ setPagina, paginaActual, tipo }) {
     const totalesHardCoded = { characters: 62, episodes: 62, deaths: 65 };
     const paginasTotal = Math.ceil(totalesHardCoded[tipo] / 10);
     
     return (
-        <div>
+        <div id="app" className="container">
+            <ul className="page">
             {[...Array(paginasTotal)].map((e, i) =>
                 paginaActual === i ? (
-                    <button disabled key={i} onClick={() => ""}>
-                        {i + 1}
-                    </button>
+                    <li className="page__numbers active" key={i} onClick={() => ""}>{i + 1}</li>
+                    // <button disabled key={i} onClick={() => ""}>
+                    //     {i + 1}
+                    // </button>
                 ) : (
-                    <button key={i} onClick={() => setPagina(i)}>
-                        {i + 1}
-                    </button>
+                    <li className="page__numbers" key={i} onClick={() => setPagina(i)}>{i + 1}</li>
+                    // <button key={i} onClick={() => setPagina(i)}>
+                    //     {i + 1}
+                    // </button>
                 )
             )}
+            
+
+    
+    
+    {/* <li class="page__numbers active">2</li>
+    <li class="page__numbers">3</li>
+    <li class="page__numbers">4</li>
+    <li class="page__numbers">5</li>
+    <li class="page__numbers">6</li> */}
+    
+    </ul>
         </div>
     );
 }
